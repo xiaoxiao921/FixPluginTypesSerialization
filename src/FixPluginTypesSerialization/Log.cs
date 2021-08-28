b09db42d96edef6d8e45e9422b962a8ddf996972
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2021-08-28 03:57:36 +0200

    Remove redundant log word

--- src/FixPluginTypesSerialization/Log.cs
+++ src/FixPluginTypesSerialization/Log.cs
@@ -11,11 +11,11 @@ namespace FixPluginTypesSerialization
             _logSource = Logger.CreateLogSource("FixPluginTypesSerialization");
         }
 
-        internal static void LogDebug(object data) => _logSource.LogDebug(data);
-        internal static void LogError(object data) => _logSource.LogError(data);
-        internal static void LogFatal(object data) => _logSource.LogFatal(data);
-        internal static void LogInfo(object data) => _logSource.LogInfo(data);
-        internal static void LogMessage(object data) => _logSource.LogMessage(data);
-        internal static void LogWarning(object data) => _logSource.LogWarning(data);
+        internal static void Debug(object data) => _logSource.LogDebug(data);
+        internal static void Error(object data) => _logSource.LogError(data);
+        internal static void Fatal(object data) => _logSource.LogFatal(data);
+        internal static void Info(object data) => _logSource.LogInfo(data);
+        internal static void Message(object data) => _logSource.LogMessage(data);
+        internal static void Warning(object data) => _logSource.LogWarning(data);
     }
 }
